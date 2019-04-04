9f8cfe418e60afe1376ae4e97ef59f9a29b903f5
Denis R <ryleew@yandex.ru>
2019-04-04 19:39:35 +0300

    first ver

--- src/main/java/io/vertx/cluster/platform/DomainGeneratorVerticle.java
+++ src/main/java/io/vertx/cluster/platform/DomainGeneratorVerticle.java
@@ -1,109 +1,107 @@
 package io.vertx.cluster.platform;
 
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Optional;
 
-import org.jsonschema2pojo.DefaultGenerationConfig;
-import org.jsonschema2pojo.GenerationConfig;
-import org.jsonschema2pojo.Jackson2Annotator;
-import org.jsonschema2pojo.SchemaGenerator;
-import org.jsonschema2pojo.SchemaMapper;
-import org.jsonschema2pojo.SchemaStore;
-import org.jsonschema2pojo.rules.RuleFactory;
-
-import com.sun.codemodel.JCodeModel;
-
+import io.swagger.codegen.v3.cli.SwaggerCodegen;
+import io.vertx.cluster.platform.util.FileRcursiveEraiser;
 import io.vertx.cluster.platform.util.GitCloneWrapper;
 import io.vertx.core.AbstractVerticle;
 
 public class DomainGeneratorVerticle extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
-        String gitUrlFrom = Optional.of(config().getString("gitUrlFrom"))
-                                    .get();
-        String gitUrlTo = Optional.of(config().getString("gitUrlTo"))
-                                  .get();
-        GitCloneWrapper.cloneUrl(gitUrlFrom);
-        GitCloneWrapper.cloneUrl(gitUrlTo);
-        generateDomain(GitCloneWrapper.calculateFolderNameFrom(gitUrlFrom), GitCloneWrapper.calculateFolderNameFrom(gitUrlTo));
-        GitCloneWrapper.pushToUrl(gitUrlTo);
-
-        vertx.setTimer(500,
-                       handler -> vertx.undeploy(deploymentID()));
-    }
-
-    private void generateDomain(String gitFolder, String generatedDomainGitUrl) throws Exception {
-        JCodeModel codeModel = new JCodeModel();
-        GenerationConfig config = createConfig();
-        SchemaMapper mapper = new SchemaMapper(new RuleFactory(config,
-                                                               new Jackson2Annotator(config),
-                                                               new SchemaStore()),
-                                               new SchemaGenerator());
-
-        Files.find(Paths.get(gitFolder),
-                   999,
-                   (path, attr) -> path.toString()
-                                       .endsWith(".json"))
-             .forEach(path -> generate(codeModel,
-                                       mapper,
-                                       path));
-
-        codeModel.build(Files.createDirectories(Paths.get(generatedDomainGitUrl +"/src"))
-                             .toFile());
-
-    }
-
-    private GenerationConfig createConfig() {
-        GenerationConfig config = new DefaultGenerationConfig() {
-            @Override
-            public boolean isGenerateBuilders() {
-                return true;
-            }
-
-            @Override
-            public boolean isIncludeJsr303Annotations() {
-                return true;
-            }
-
-            @Override
-            public boolean isUseTitleAsClassname() {
-                return true;
-            }
-
-            @Override
-            public boolean isIncludeAdditionalProperties() {
-                return false;
-            }
-
-            @Override
-            public String getDateTimeType() {
-                return "java.time.LocalDateTime";
-            }
-
-            @Override
-            public String getDateType() {
-                return "java.time.LocalDate";
-            }
-
-        };
-        return config;
-    }
-
-    private void generate(JCodeModel codeModel, SchemaMapper mapper, Path path) {
+    public void start() {
         try {
-            mapper.generate(codeModel,
-                            "ClassName",
-                            "io.vertx.cluster.platform.domain",
-                            path.toUri()
-                                .toURL());
-        } catch (MalformedURLException e) {
+            String gitUrlFrom = Optional.of(config().getString("gitUrlFrom"))
+                                        .get();
+            String gitUrlTo = Optional.of(config().getString("gitUrlTo"))
+                                      .get();
+            GitCloneWrapper.cloneUrl(gitUrlFrom);
+            GitCloneWrapper.cloneUrl(gitUrlTo);
+            String outputGitFolderName = GitCloneWrapper.calculateFolderNameFrom(gitUrlTo);
+            generateDomain(GitCloneWrapper.calculateFolderNameFrom(gitUrlFrom),
+                           outputGitFolderName);
+            vertx.setPeriodic(200,
+                              i -> vertx.fileSystem()
+                                        .exists(outputGitFolderName + "/.swagger-codegen/VERSION",
+                                                h -> {
+                                                    System.out.println("XXXXXXXXXXXXXXXXX "+ h);
+                                                    if(!h.result()) {
+                                                        return;
+                                                    }
+                                                    GitCloneWrapper.pushToUrl(gitUrlTo);
+                                                    vertx.setTimer(500,
+                                                                   handler -> vertx.undeploy(deploymentID()));
+                                                    vertx.cancelTimer(i);
+                                                }));
+
+        } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
+
+    private void generateDomain(String gitFolder, String generatedDomainGitUrl) throws Exception {
+        FileRcursiveEraiser.deleteRecursive(generatedDomainGitUrl, true);
+
+        String[] args2 = new String[12];
+        args2[0] = "generate";
+        args2[1] = "-l";
+        args2[2] = "java-vertx";
+        args2[3] = "-i";
+        args2[4] = gitFolder + "/swagger.yaml";
+        args2[5] = "-o";
+        args2[6] = generatedDomainGitUrl;
+        args2[7] = "--group-id";
+        args2[8] = "io.vertx.cluster.platform";
+        args2[9] = "--artifact-id";
+        args2[10] = "domain-rx";
+        args2[11] = "-DrxInterface=true,apiImplGeneration=true";
+        SwaggerCodegen.main(args2);
+
+    }
+
+    /*
+     * private void generateDomain(String gitFolder, String generatedDomainGitUrl)
+     * throws Exception { FileRcursiveEraiser.deleteRecursive(generatedDomainGitUrl
+     * +"/src");
+     * 
+     * JCodeModel codeModel = new JCodeModel(); GenerationConfig config =
+     * createConfig(); SchemaMapper mapper = new SchemaMapper(new
+     * VertxPlatformRuleFactory(config, new Jackson2Annotator(config), new
+     * SchemaStore()), new SchemaGenerator());
+     * 
+     * Files.find(Paths.get(gitFolder), 999, (path, attr) -> path.toString()
+     * .endsWith(".json")) .forEach(path -> generate(codeModel, mapper, path));
+     * 
+     * codeModel.build(Files.createDirectories(Paths.get(generatedDomainGitUrl
+     * +"/src")) .toFile());
+     * 
+     * }
+     * 
+     * private GenerationConfig createConfig() { GenerationConfig config = new
+     * DefaultGenerationConfig() {
+     * 
+     * @Override public boolean isGenerateBuilders() { return true; }
+     * 
+     * @Override public boolean isIncludeJsr303Annotations() { return true; }
+     * 
+     * @Override public boolean isUseTitleAsClassname() { return true; }
+     * 
+     * @Override public boolean isIncludeAdditionalProperties() { return false; }
+     * 
+     * @Override public String getDateTimeType() { return "java.time.LocalDateTime";
+     * }
+     * 
+     * @Override public String getDateType() { return "java.time.LocalDate"; }
+     * 
+     * }; return config; }
+     * 
+     * private void generate(JCodeModel codeModel, SchemaMapper mapper, Path path) {
+     * try { mapper.generate(codeModel, "ClassName",
+     * "io.vertx.cluster.platform.domain", path.toUri() .toURL()); } catch
+     * (MalformedURLException e) { throw new RuntimeException(e); } }
+     */
 
     @Override
     public void stop() throws Exception {
---
+++ src/main/java/io/vertx/cluster/platform/util/FileRcursiveEraiser.java
@@ -0,0 +1,24 @@
+package io.vertx.cluster.platform.util;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+public class FileRcursiveEraiser {
+    public static void deleteRecursive(String folderToDelete, boolean skipGitServiceFolder) throws Exception {
+        System.out.println("DELETE");
+        Path path = Paths.get(folderToDelete);
+        if (path.toFile()
+                .exists()) {
+            Files.walk(path)
+                 .filter(currPath -> skipGitServiceFolder ? !currPath.startsWith(folderToDelete + "/.git") : true)
+                 .peek(System.out::println)
+                 .sorted(Comparator.reverseOrder())
+                 .map(Path::toFile)
+                 .forEach(File::delete);
+        }
+    }
+
+}
